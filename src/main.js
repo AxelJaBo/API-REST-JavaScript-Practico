a70b6afd7b11da53fbb24f4be65e815d01c8536b
Daniel <axel.910921@gmail.com>
2023-06-29 17:59:25 -0700

    Deploy 4

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 const APIKEY = process.env.API_KEY;
 const URL = 'https://api.themoviedb.org/3/';
 const api = axios.create({
